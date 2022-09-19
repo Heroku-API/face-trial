352b659052edcbe7e4657201a973ca622c176a01
unknown <madhusai14april@gmail.com>
2022-09-19 12:53:44 +0530

    committed

---
+++ app.py
@@ -0,0 +1,19 @@
+from face_copy_flask import *
+from flask import Flask,Response
+app = Flask(__name__)
+
+@app.route("/")
+def run_stream():
+    source=r'C:\Users\Keerthika\Desktop\face_recognition\input_1.mp4'
+
+
+
+
+    return Response(main(source), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8087)
